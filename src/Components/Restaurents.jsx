86437916f8868e1f6a7c186746a5d218a22affa8
Ujjwal Vishnoi <ujjwalvishnoi62@gmail.com>
2026-06-09 17:17:36 +0530

    fix vercel production deployment

--- src/Components/Restaurents.jsx
+++ src/Components/Restaurents.jsx
@@ -1,148 +1,199 @@
-import { Routes, Route } from "react-router-dom"
-import Navvar from "./Navvar"
+import { Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 
-import { useDispatch, useSelector } from "react-redux"
-import { useState, useEffect } from "react"
+import Navvar from "./Navvar";
 
-import SwiggyCorporate from "./Navvar_Element/SwiggyCorporate"
-import Search from "./Navvar_Element/Search"
-import Offers from "./Navvar_Element/Offers"
-import Help from "./Navvar_Element/Help"
-import SignIn from "./Navvar_Element/SignIn"
-import Cart from "./Navvar_Element/Cart"
+import SwiggyCorporate from "./Navvar_Element/SwiggyCorporate";
+import Search from "./Navvar_Element/Search";
+import Offers from "./Navvar_Element/Offers";
+import Help from "./Navvar_Element/Help";
+import SignIn from "./Navvar_Element/SignIn";
+import Cart from "./Navvar_Element/Cart";
 
-import Loder from "./Loder"
-import FoodSuggestion from "./FoodSuggestion"
-import TopReastaurents from "./TopRestaurents"
-import RestaurentInArea from "./RestaurentInArea"
+import Loder from "./Loder";
+import FoodSuggestion from "./FoodSuggestion";
+import TopReastaurents from "./TopRestaurents";
+import RestaurentInArea from "./RestaurentInArea";
 
-import { addHomeData } from "../Utilis/CacheSlice"
+import { addHomeData } from "../Utilis/CacheSlice";
 
 
 function Restaurents() {
 
-    const [suggestionData, setSuggestionData] = useState({})
-    const [topRestaurents, setTopRestaurents] = useState({})
-    const [restaurentInArea, setRestaurentInArea] = useState({})
+  const [suggestionData, setSuggestionData] = useState({});
+  const [topRestaurents, setTopRestaurents] = useState({});
+  const [restaurentInArea, setRestaurentInArea] = useState({});
+  const [error, setError] = useState(false);
 
-    const location = useSelector(store => store.location)
-    const cacheData = useSelector(store => store.cache.home)
+  const location = useSelector(store => store.location);
+  const cacheData = useSelector(store => store.cache.home);
 
-    const dispatch = useDispatch()
-
-
-    function filterData(apiData) {
-
-        const cards = apiData?.data?.cards
-
-        if (!cards) return
+  const dispatch = useDispatch();
 
 
-        // FOOD SUGGESTION
-        const suggestion =
-            cards.find(
-                item =>
-                    item?.card?.card?.imageGridCards
-            )
+  const filterData = (apiData) => {
 
-        setSuggestionData({
+    const cards = apiData?.data?.cards;
 
-            title:
-                suggestion?.card?.card?.header?.title
-                ||
-                "What's on your mind?",
-
-            card:
-                suggestion?.card?.card?.imageGridCards?.info || []
-
-        })
+    if (!cards) return;
 
 
-        // TOP RESTAURANTS
+    // What's on your mind
 
-        const restaurantCards =
-            cards.find(
-                item =>
-                    item?.card?.card?.gridElements
-                    ?.infoWithStyle?.restaurants
-            )
+    const suggestion = cards.find(
+      item =>
+        item?.card?.card?.imageGridCards
+    );
 
 
-        const restaurants =
-            restaurantCards
-            ?.card
-            ?.card
-            ?.gridElements
-            ?.infoWithStyle
-            ?.restaurants
-            ?.map(item => item.info)
+    setSuggestionData({
 
+      title:
+        suggestion?.card?.card?.header?.title ||
+        "What's on your mind?",
 
-        setTopRestaurents({
+      card:
+        suggestion
+        ?.card
+        ?.card
+        ?.imageGridCards
+        ?.info || []
 
-            title:
-            restaurantCards
-            ?.card
-            ?.card
-            ?.header
-            ?.title
-            ||
-            "Top Restaurants",
-
-            cards: restaurants || []
-
-        })
-
-
-        setRestaurentInArea({
-
-            title:"Restaurants in your area",
-
-            cards: restaurants || []
-
-        })
-
-    }
+    });
 
 
 
-    useEffect(() => {
+    // Top Restaurants
+
+    const restaurantCards = cards.find(
+      item =>
+        item
+        ?.card
+        ?.card
+        ?.gridElements
+        ?.infoWithStyle
+        ?.restaurants
+    );
 
 
-        if(cacheData){
-
-            filterData(cacheData)
-
-        }
-
-        else if(location?.data?.lat){
-
-
-            fetch(
-`https://www.swiggy.com/dapi/restaurants/list/v5?lat=${location.data.lat}&lng=${location.data.lon}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
-            )
-
-            .then(res => res.json())
-
-            .then((data)=>{
-
-                dispatch(addHomeData(data))
-
-                filterData(data)
-
-            })
-
-            .catch(err =>{
-
-                console.log("API ERROR",err)
-
-            })
+    const restaurants =
+      restaurantCards
+      ?.card
+      ?.card
+      ?.gridElements
+      ?.infoWithStyle
+      ?.restaurants
+      ?.map(item => item.info) || [];
 
 
-        }
+    setTopRestaurents({
+
+      title:
+        restaurantCards
+        ?.card
+        ?.card
+        ?.header
+        ?.title
+        ||
+        "Top restaurant chains",
+
+      cards: restaurants
+
+    });
 
 
-    },[location,cacheData,dispatch])
+    setRestaurentInArea({
+
+      title:
+      "Restaurants in your area",
+
+      cards: restaurants
+
+    });
+
+  };
+
+
+
+useEffect(() => {
+
+
+if(cacheData){
+
+  filterData(cacheData);
+  return;
+
+}
+
+
+if(location?.data?.lat){
+
+
+const API = 
+`https://www.swiggy.com/dapi/restaurants/list/v5?lat=${location.data.lat}&lng=${location.data.lon}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
+
+
+
+fetch(API)
+
+.then(response => {
+
+if(!response.ok){
+throw new Error("API Failed");
+}
+
+return response.json();
+
+})
+
+
+.then(data => {
+
+dispatch(addHomeData(data));
+
+filterData(data);
+
+})
+
+
+.catch(error => {
+
+console.log("Swiggy API Error:",error);
+
+setError(true);
+
+});
+
+
+}
+
+
+},[location,cacheData,dispatch]);
+
+
+
+
+
+if(error){
+
+return(
+
+<div>
+
+<Navvar/>
+
+<h2 className="text-center mt-20 text-xl">
+
+Something went wrong. Please try again.
+
+</h2>
+
+</div>
+
+)
+
+}
 
 
 
@@ -151,39 +202,86 @@ return (
 
 <div>
 
+
 <Navvar/>
 
 
 <Routes>
 
-<Route path="search" element={<Search/>}/>
-<Route path="offers" element={<Offers/>}/>
-<Route path="help" element={<Help/>}/>
-<Route path="signin" element={<SignIn/>}/>
-<Route path="cart" element={<Cart/>}/>
+<Route 
+path="corporate" 
+element={<SwiggyCorporate/>}
+/>
+
+<Route 
+path="search" 
+element={<Search/>}
+/>
+
+<Route 
+path="offers" 
+element={<Offers/>}
+/>
+
+<Route 
+path="help" 
+element={<Help/>}
+/>
+
+<Route 
+path="signin" 
+element={<SignIn/>}
+/>
+
+<Route 
+path="cart" 
+element={<Cart/>}
+/>
 
 </Routes>
 
 
+
+
 {
-suggestionData?.title ?
+suggestionData?.card?.length > 0 ?
 
 <div>
 
 
-<FoodSuggestion data={suggestionData}/>
+<FoodSuggestion 
+data={suggestionData}
+/>
 
 
-<hr className="border border-gray-200 w-[80vw] mx-auto mt-10"/>
+<hr className="
+border border-gray-200 
+w-[80vw] 
+mx-auto 
+mt-10"
+/>
 
 
-<TopReastaurents data={topRestaurents}/>
+
+<TopReastaurents 
+data={topRestaurents}
+/>
 
 
-<hr className="border border-gray-200 w-[80vw] mx-auto mt-10"/>
+
+<hr className="
+border border-gray-200 
+w-[80vw] 
+mx-auto 
+mt-10"
+/>
 
 
-<RestaurentInArea data={restaurentInArea}/>
+
+<RestaurentInArea
+data={restaurentInArea}
+/>
+
 
 
 </div>
@@ -192,7 +290,12 @@ suggestionData?.title ?
 :
 
 
-<div className="h-screen flex justify-center items-center">
+<div className="
+h-screen 
+flex 
+justify-center 
+items-center"
+>
 
 <Loder/>
 
@@ -207,4 +310,5 @@ suggestionData?.title ?
 
 }
 
-export default Restaurents
+
+export default Restaurents;
--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
 
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://www.swiggy.com",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+export default defineConfig({
+
+plugins:[
+
+react(),
+
+tailwindcss()
+
+]
+
 })

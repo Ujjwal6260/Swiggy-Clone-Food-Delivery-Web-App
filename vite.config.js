6c545d0a94a6ed41d9786ccefc2abb2288628b3b
Ujjwal Vishnoi <ujjwalvishnoi62@gmail.com>
2026-06-09 16:56:14 +0530

    fix restaurant card images on vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    react(),
+    tailwindcss()
   ],
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://www.swiggy.com",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 })
